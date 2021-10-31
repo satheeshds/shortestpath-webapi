06fca57845d7af8cd51c65252dc96b2e801fe5b1
Satheesh <ssabariyamma@talabat.com>
2021-10-31 20:32:54 +0400

    Template projects added

---
+++ src/shortestpath-webapi-test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace shortestpath_webapi_test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
